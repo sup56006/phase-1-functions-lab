c07f7f21a2e66513efbc3bd8e9f38e1fbc326926
Isaac Kyuma <isaackyuma56@gmail.com>
2025-09-09 09:00:49 +0300

    complete scuber distance and fare functions

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Code your solution in this file!
+// 1. Distance from HQ in blocks
+function distanceFromHqInBlocks(someValue) {
+  return Math.abs(someValue - 42);
+}
+
+// 2. Distance from HQ in feet
+function distanceFromHqInFeet(someValue) {
+  return distanceFromHqInBlocks(someValue) * 264;
+}
+
+// 3. Distance traveled in feet
+function distanceTravelledInFeet(start, destination) {
+  return Math.abs(destination - start) * 264;
+}
+
+// 4. Calculate fare price
+function calculatesFarePrice(start, destination) {
+  const distance = distanceTravelledInFeet(start, destination);
+
+  if (distance <= 400) {
+    return 0;
+  } else if (distance <= 2000) {
+    return (distance - 400) * 0.02;
+  } else if (distance <= 2500) {
+    return 25;
+  } else {
+    return 'cannot travel that far';
+  }
+}
+
